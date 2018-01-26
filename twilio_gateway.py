9eff6ca08c97f910eae90cc438eaca6e88734705
Oliver Steele <steele@osteele.com>
2018-01-26 10:46:27 -0500

    Serve up a nicer hello page

--- twilio_gateway.py
+++ twilio_gateway.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 
-from flask import Flask, request
+from flask import Flask, render_template
 from twilio.twiml.messaging_response import MessagingResponse
 
 from send_mqtt_messages import publish
@@ -14,14 +14,14 @@ HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
 PORT = int(os.environ.get('PORT', 5000))
 FLASK_DEBUG = os.environ.get('FLASK_DEBUG') or 'PORT' not in os.environ
 RESPONSE_TEXT = os.environ.get('RESPONSE_TEXT')
-GITHUB_URL = 'https://github.com/olinlibrary/bear-as-a-service'
+GITHUB_README_URL = 'https://github.com/olin-build/twilio-mqtt-gateway#README'
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return f'The server is running. <a href="{GITHUB_URL}">More…</a>'
+    return render_template('index.html', readme_url=GITHUB_README_URL)
 
 
 @app.route('/sms_webhook', methods=['POST'])
